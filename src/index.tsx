979b9d73c07790bc4c0d3c43bc6de564f8860226
Mijung <mijung.m@gmail.com>
2019-04-29 15:06:26 -0700

    adds styled components & removes boilerplate css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+// Third-party imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// Local imports
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/styles/Constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Style constants
+ **/
+
+export const white = '#fefefe';
