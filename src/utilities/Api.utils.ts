04b527c5fc81775ac1f77cfd5c4290a34fe26cfc
Mijung <mijung.m@gmail.com>
2019-04-30 09:39:17 -0700

    adds further type interfaces and api fetches

--- src/utilities/Api.utils.ts
+++ src/utilities/Api.utils.ts
@@ -22,3 +22,33 @@ export const getTransactions = async () => {
   const data = (await response.json()) as TransactionsResponse;
   return data;
 };
+
+interface Account {
+  accountId: string;
+  institutionName: string;
+  accountName: string;
+  transitNumber: string;
+  accountNumber: string;
+  balance: number;
+  balanceUpdated: string;
+}
+
+interface AccountsResponse {
+  accounts: Account[];
+}
+
+export const getAccounts = async () => {
+  const response = await fetch(`https://demo1124891.mockable.io/accounts`);
+  const data = (await response.json()) as AccountsResponse;
+  return data;
+};
+
+interface CategoriesResponse {
+  categories: string[];
+}
+
+export const getCategories = async () => {
+  const response = await fetch(`https://demo1124891.mockable.io/categories`);
+  const data = (await response.json()) as CategoriesResponse;
+  return data;
+};
