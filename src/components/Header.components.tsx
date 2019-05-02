cf590e7f7b0d355f5ebd2655a48aaf976901f5ce
Mijung <mijung.m@gmail.com>
2019-05-02 12:54:40 -0700

    adds institution logo

--- src/components/Header.components.tsx
+++ src/components/Header.components.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 // Local imports
+import piggy from '../assets/piggy-bank.svg';
 
 const HeaderContainer = styled.div`
   align-items: flex-end;
@@ -15,15 +16,22 @@ const HeaderContainer = styled.div`
 const InstitutionName = styled.div`
   color: white;
   font-family: 'Heebo', serif;
-  font-size: calc(13px + 3vmin);
-
+  /* font-size: calc(13px + 3vmin); */
+  font-size: 38px;
   font-weight: 800;
   letter-spacing: 0;
+  line-height: 38px;
+`;
+
+const InstitutionLogo = styled.img`
+  height: 55px;
+  margin-right: 15px;
 `;
 
 export const Header = () => {
   return (
     <HeaderContainer>
+      <InstitutionLogo src={piggy} alt="logo" />
       <InstitutionName>Worldwide Savings Bank</InstitutionName>
     </HeaderContainer>
   );
